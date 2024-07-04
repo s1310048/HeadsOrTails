85feda46148874e9e0972ed23010c710e3af5608
稲生一星 <inaohajimesei@wlan-napt-005.u-aizu.ac.jp>
2024-07-04 13:14:01 +0900

    User name added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -1,7 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class HeadsOrTails {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("あなたは誰ですか？");
+        String userName = scanner.nextLine();
+        System.out.println("こんにちは、" + userName);
+
         String[] outcomes = {"表", "裏"};
         int headsCount = 0;
         int tailsCount = 0;
